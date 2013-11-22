f53a948408ddaa63d426b7b7dc85a61786b51e71
davidBastos <davtechh@gmail.com>
2013-11-21 22:44:33 -0300

    setando configurações de conexão do banco de dados para salvar os e-mails dos clientes

--- save.php
+++ save.php
@@ -4,7 +4,7 @@
     $email = $_POST['email'];
   }
   
-  $db = mysql_connect("localhost", "database_name", "database_password"); 
-  mysql_select_db("database_name", $db);
+  $db = mysql_connect("dbmy0012.whservidor.com", "comercialb43", "GrupoAnalisi01"); 
+  mysql_select_db("comercialb43", $db);
   mysql_query("INSERT INTO address (email) VALUES ('$email')", $db);
 ?>
